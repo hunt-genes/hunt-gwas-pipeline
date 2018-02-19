a1e063185fe6925f90f7e9300d094fa5e0728d5b
Endre Bakken Stovner <endrebak@dhcp-10-24-81-159.wlan.ntnu.no>
2018-02-19 13:06:06 +0100

    Fix titles plots when pheno continuous

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,26 +1,29 @@
 import re
 
 
-def trait_type(pk, w):
-
-    binary = pk[w.pheno].type == "binary"
-    trait_type = "binary" if binary else "quantitative"
-
-    return trait_type
-
 
 def title(pk, w):
 
-    binary = pk.loc[pk.name == w.pheno].type.iloc[0] == "binary"
-    trait_type = "binary" if binary else "quantitative"
-    n_cases = pk.loc[pk.name == w.pheno].N_cases.iloc[0]
+    trait_type = pk.loc[pk.name == w.pheno].type.iloc[0]
     trait = w.pheno
 
-    n_controls = pk.loc[pk.name == w.pheno].N_controls.iloc[0] if binary else 0
+    if trait_type == "binary":
 
-    title = "".join([str(s) for s in ["Phenotype: ", trait, " Type: ",
-                                      trait_type + "\n(", n_cases, " cases versus ",
-                                      n_controls, " controls)"]])
+        n_cases = pk.loc[pk.name == w.pheno].N_cases.iloc[0]
+
+        n_controls = pk.loc[pk.name == w.pheno].N_controls.iloc[0]
+
+        title = "".join([str(s) for s in ["Phenotype: ", trait, " Type: ",
+                                        trait_type + "\n(", n_cases, " cases versus ",
+                                        n_controls, " controls)"]])
+
+    elif trait_type == "quantitative":
+
+        n = pk.loc[pk.name == w.pheno].N.iloc[0]
+
+        title = "".join([str(s) for s in ["Phenotype: ", trait, " Type: ",
+                                          trait_type + ", N=", n]])
+
 
     return title
 
